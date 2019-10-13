cdeff715557802a33ec100c835f18e9cb326109e
Dawid Kicinski <kicinski.dawid@gmail.com>
2019-10-14 01:00:28 +0200

    Change visibility of model attribute in EdgeDevice to private

--- src/federated-faceid/examples/mnist/models/federated.py
+++ src/federated-faceid/examples/mnist/models/federated.py
@@ -34,46 +34,45 @@ class EdgeDevice:
     def __init__(self, device_id: int, settings: EdgeDeviceSettings, subset: Subset):
         self.device_id = device_id
         self.setting = settings
-        self.loss_func = CrossEntropyLoss()
-        # self.subset: Subset = subset
-        self.data_loader = DataLoader(subset.dataset,
-                                      sampler=SubsetRandomSampler(subset.indices),
-                                      batch_size=self.setting.batch_size)
+        self._loss_func = CrossEntropyLoss()
+        self._data_loader = DataLoader(subset.dataset,
+                                       sampler=SubsetRandomSampler(subset.indices),
+                                       batch_size=self.setting.batch_size)
 
-        self.model: Optional[Module] = None
+        self._model: Optional[Module] = None
 
     def download(self, model: Module):
-        self.model = copy.deepcopy(model)
+        self._model = copy.deepcopy(model)
 
     def upload(self) -> Module:
-        if self.model is not None:
-            return copy.deepcopy(self.model)
+        if self._model is not None:
+            return copy.deepcopy(self._model)
         else:
             raise ValueError("Model not found on this device!")
 
     def train(self, verbose: bool = False) -> float:
-        if self.data_loader is None:
+        if self._data_loader is None:
             raise ValueError("Dataset not found on this device!")
 
-        self.model.train()
-        optimizer = torch.optim.SGD(params=self.model.parameters(),
+        self._model.train()
+        optimizer = torch.optim.SGD(params=self._model.parameters(),
                                     lr=self.setting.learning_rate,
                                     momentum=0.5)
 
         epoch_loss = []
         for i_epoch in range(self.setting.epochs):
             batch_loss = []
-            for i_batch, (images, labels) in enumerate(self.data_loader):
+            for i_batch, (images, labels) in enumerate(self._data_loader):
                 images, labels = images.to(self.setting.device), labels.to(self.setting.device)
-                self.model.zero_grad()
-                log_probs = self.model(images)
-                loss = self.loss_func(log_probs, labels)
+                self._model.zero_grad()
+                log_probs = self._model(images)
+                loss = self._loss_func(log_probs, labels)
                 loss.backward()
                 optimizer.step()
                 if verbose and i_batch % 10 == 0:
                     print('Update Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'
-                          .format(i_batch, i_batch * len(images), len(self.data_loader),
-                                  100.0 * i_batch / len(self.data_loader), loss.item()))
+                          .format(i_batch, i_batch * len(images), len(self._data_loader),
+                                  100.0 * i_batch / len(self._data_loader), loss.item()))
                 batch_loss.append(loss.item())
             epoch_loss.append(sum(batch_loss) / len(batch_loss))
         return sum(epoch_loss) / len(epoch_loss)
