41a02632a3c81203d314dc744ece4ea880548a3f
Dawid Kicinski <kicinski.dawid@gmail.com>
2019-11-03 18:33:36 +0100

    Add evaluation script; save evaluation results

--- src/federated-faceid/examples/mnist/evaluate_mnist.py
+++ src/federated-faceid/examples/mnist/evaluate_mnist.py
@@ -1,27 +1,17 @@
-import shutil
-
 import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import CIFAR10
 
-import training
 from models.baseline import CNNCifar10
-from training.evaluation import evaluate, EvaluationResult
+from training import evaluation
+from training.evaluation import EvaluationResult
 from utils import constants
 from utils.settings import Settings, args_parser
 
 
-def create_paths():
-    shutil.rmtree(str(constants.PATH_OUTPUT_MODEL_SERVER))
-    shutil.rmtree(str(constants.PATH_OUTPUT_MODEL_FEDERATED))
-    constants.PATH_OUTPUT_MODEL_SERVER.mkdir(exist_ok=True, parents=True)
-    constants.PATH_OUTPUT_MODEL_FEDERATED.mkdir(exist_ok=True, parents=True)
-
-
-def train():
+def evaluate():
     # parse args
-    create_paths()
     settings: Settings = args_parser()
     settings.device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu')
     torch.manual_seed(settings.seed)
@@ -29,8 +19,6 @@ def train():
     # load dataset and split users
     transform = transforms.Compose([transforms.ToTensor(),
                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    dataset_train = CIFAR10(constants.PATH_DATASET_CIFAR10,
-                            train=True, transform=transform, download=True)
 
     model: torch.nn.Module = CNNCifar10()
     model.to(settings.device)
@@ -39,17 +27,18 @@ def train():
                            train=False, transform=transform, download=True)
 
     if settings.distributed:
-        training.train_federated(model, dataset_train, settings)
         save_path = constants.PATH_OUTPUT_MODEL_FEDERATED
+        model.load_state_dict(torch.load(save_path.joinpath("model.pt")))
     else:
-        training.train_server(model, dataset_train, dataset_test, settings)
         save_path = constants.PATH_OUTPUT_MODEL_SERVER
+        model.load_state_dict(torch.load(save_path.joinpath("model.pt")))
 
     test_loader = DataLoader(dataset_test, batch_size=settings.num_global_batch, shuffle=False)
-    result: EvaluationResult = evaluate(model.cpu(), test_loader, verbose=True)
+    result: EvaluationResult = evaluation.evaluate(model.cpu(), test_loader, verbose=True)
+
     with save_path.joinpath("evaluation.txt").open("w") as f:
         f.write(str(result))
 
 
 if __name__ == '__main__':
-    train()
+    evaluate()
